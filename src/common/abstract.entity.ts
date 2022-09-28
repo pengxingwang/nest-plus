8305759677d0ca6750458b6b97a0c1d7abcb08b6
pxw <939212596@qq.com>
2022-09-28 18:28:45 +0800

    feat: update

---
+++ src/common/abstract.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class AbstractEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text', nullable: true })
+  remark: string;
+
+  @CreateDateColumn({ name: 'create_time' })
+  createTime: Date;
+
+  @UpdateDateColumn({ name: 'update_time' })
+  updateTime: Date;
+
+  @DeleteDateColumn({ name: 'delete_time' })
+  deleteTime: Date;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
-import compression from '@fastify/compress';
-import helmet from '@fastify/helmet';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-    { cors: true, bufferLogs: true },
-  );
-
-  await app.register(helmet);
-  await await app.register(compression, { encodings: ['gzip', 'deflate'] });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bufferLogs: true,
+  });
 
   await app.useLogger(app.get(Logger));
 
